68690d1b3645adba5b27e7e28568b772616a1624
John Terry <92858448+Johnterrie@users.noreply.github.com>
2023-07-14 08:09:29 +0100

    Update auth-workflow.js

--- middlewares/auth-workflow.js
+++ middlewares/auth-workflow.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const secretkey = "John terry";
+const secretkey = "";
 
 const verifyToken = (req, res, next) => {
     const token = req.headers["authorization"];
